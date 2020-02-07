355791ed8e892824f234aa71fdc3aa6cf84834b1
Kelvin Liang <kelvin9877@gmail.com>
2020-02-08 03:02:30 +0800

    finish milestone 2

--- src/components/button.js
+++ src/components/button.js
@@ -3,11 +3,14 @@ import PropTypes from 'prop-types';
 
 const Button = props => {
   const { name, onClick } = props;
+  const btnClass = name === '0' ? "button zero" : "button";
+  const sideButtons = ['÷', 'x', '-', '+', '=']
+  const colorClass = sideButtons.includes(name) ? "background-orange" : "background-white"
 
   return (
     <button
       type="button"
-      className="button"
+      className={btnClass + ' ' + colorClass}
       onClick={onClick}
     >
       {name}
